295e04d6701e5b944c5e140fdb846f1e56cde7df
malith damsara <malithdamsara87@gmail.com>
2025-01-13 17:19:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HelloWorldApp());
+}
+
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('project one'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello malithsdfghjkl',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
